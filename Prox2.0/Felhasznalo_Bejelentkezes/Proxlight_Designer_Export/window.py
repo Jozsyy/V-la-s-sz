d82c9b2085f4b7454a04a23b4c2ebf7c0dcd63ea
S.Illes <sarkany.illes@student.ms.sapientia.ro>
2022-10-31 22:44:21 +0200

    Exporting Wireframe 2

---
+++ Prox2.0/Felhasznalo_Bejelentkezes/Proxlight_Designer_Export/window.py
@@ -0,0 +1,84 @@
+from tkinter import *
+
+
+def btn_clicked():
+    print("Button Clicked")
+
+
+window = Tk()
+
+window.geometry("1080x607")
+window.configure(bg = "#ffffff")
+canvas = Canvas(
+    window,
+    bg = "#ffffff",
+    height = 607,
+    width = 1080,
+    bd = 0,
+    highlightthickness = 0,
+    relief = "ridge")
+canvas.place(x = 0, y = 0)
+
+background_img = PhotoImage(file = f"background.png")
+background = canvas.create_image(
+    540.0, 303.5,
+    image=background_img)
+
+entry0_img = PhotoImage(file = f"img_textBox0.png")
+entry0_bg = canvas.create_image(
+    540.0, 304.0,
+    image = entry0_img)
+
+entry0 = Entry(
+    bd = 0,
+    bg = "#d9d9d9",
+    highlightthickness = 0)
+
+entry0.place(
+    x = 388, y = 272,
+    width = 304,
+    height = 62)
+
+entry1_img = PhotoImage(file = f"img_textBox1.png")
+entry1_bg = canvas.create_image(
+    540.0, 459.0,
+    image = entry1_img)
+
+entry1 = Entry(
+    bd = 0,
+    bg = "#d9d9d9",
+    highlightthickness = 0)
+
+entry1.place(
+    x = 388, y = 427,
+    width = 304,
+    height = 62)
+
+img0 = PhotoImage(file = f"img0.png")
+b0 = Button(
+    image = img0,
+    borderwidth = 0,
+    highlightthickness = 0,
+    command = btn_clicked,
+    relief = "flat")
+
+b0.place(
+    x = 444, y = 508,
+    width = 191,
+    height = 65)
+
+img1 = PhotoImage(file = f"img1.png")
+b1 = Button(
+    image = img1,
+    borderwidth = 0,
+    highlightthickness = 0,
+    command = btn_clicked,
+    relief = "flat")
+
+b1.place(
+    x = 667, y = 523,
+    width = 178,
+    height = 50)
+
+window.resizable(False, False)
+window.mainloop()
