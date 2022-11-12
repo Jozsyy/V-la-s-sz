adef6286236452552d39113605c0fa551da7cdb9
Bartha-coder <bartha.robert.dezso@student.ms.sapientia.ro>
2022-11-12 18:48:57 +0200

    update

--- SRC/Main/Proxlight_Designer_Export/window.py
+++ SRC/Main/Proxlight_Designer_Export/window.py
@@ -583,43 +583,43 @@ def sikeresRegisztracio():
 
 def kerdes(): #Robi
     def selfDestroy():
-        Kerdes_Vege_Button.destroy()
-        Kerdes_Mehet_Button.destroy()
+    Kerdes_Vege_Button.destroy()
+    Kerdes_Mehet_Button.destroy()
 
 
-        background_img = PhotoImage(file=f"Kerdes_Background.png")
-        background = canvas.create_image(
+    background_img = PhotoImage(file=f"Kerdes_Background.png")
+    background = canvas.create_image(
             540.0, 303.5,
             image=background_img)
 
-        img0 = PhotoImage(file=f"Kerdes_Vege.png")
-         Kerdes_Vege_Button= Button(
+    img0 = PhotoImage(file=f"Kerdes_Vege.png")
+    Kerdes_Vege_Button= Button(
             image=img0,
             borderwidth=0,
             highlightthickness=0,
             command=btn_clicked,
             relief="flat")
 
-        Kerdes_Vege_Button.place(
+    Kerdes_Vege_Button.place(
             x=894, y=547,
             width=173,
             height=46)
 
-        img1 = PhotoImage(file=f"Kerdes_Mehet.png")
-        Kerdes_Mehet_Button = Button(
+    img1 = PhotoImage(file=f"Kerdes_Mehet.png")
+    Kerdes_Mehet_Button = Button(
             image=img1,
             borderwidth=0,
             highlightthickness=0,
             command=btn_clicked,
             relief="flat")
 
-        Kerdes_Mehet_Button.place(
+    Kerdes_Mehet_Button.place(
             x=426, y=497,
             width=228,
             height=96)
 
-        window.resizable(False, False)
-        window.mainloop()
+    window.resizable(False, False)
+    window.mainloop()
 
 def jatsz():  #Robi
     def selfDestroy():
@@ -663,57 +663,57 @@ def jatsz():  #Robi
 
 def kategoriak_kivalaszt():  #Robi
     def selfDestroy():
-        Kategoriak_Vissza_Button.destroy()
-        Kategoriak_Szerkeszt_Button.destroy()
-        Kategoriak_Inditas_Button.destroy()
+    Kategoriak_Vissza_Button.destroy()
+    Kategoriak_Szerkeszt_Button.destroy()
+    Kategoriak_Inditas_Button.destroy()
 
 
-        background_img = PhotoImage(file=f"Kategoriak_Background.png")
-        background = canvas.create_image(
+    background_img = PhotoImage(file=f"Kategoriak_Background.png")
+     background = canvas.create_image(
             540.0, 303.5,
             image=background_img)
 
-        img0 = PhotoImage(file=f"Kategoriak_Vissza.png")
-        Kategoriak_Vissza_Button = Button(
+    img0 = PhotoImage(file=f"Kategoriak_Vissza.png")
+    Kategoriak_Vissza_Button = Button(
             image=img0,
             borderwidth=0,
             highlightthickness=0,
             command=btn_clicked,
             relief="flat")
 
-        Kategoriak_Vissza_Button.place(
+    Kategoriak_Vissza_Button.place(
             x=894, y=543,
             width=178,
             height=50)
 
-        img1 = PhotoImage(file=f"Kategoriak_Szerkeszt.png")
-        Kategoriak_Szerkeszt_Button = Button(
+    img1 = PhotoImage(file=f"Kategoriak_Szerkeszt.png")
+    Kategoriak_Szerkeszt_Button = Button(
             image=img1,
             borderwidth=0,
             highlightthickness=0,
             command=btn_clicked,
             relief="flat")
 
-        Kategoriak_Szerkeszt_Button.place(
+    Kategoriak_Szerkeszt_Button.place(
             x=894, y=368,
             width=142,
             height=37)
 
-        img2 = PhotoImage(file=f"Kategoriak_Inditas.png")
-        Kategoriak_Inditas_Button = Button(
+    img2 = PhotoImage(file=f"Kategoriak_Inditas.png")
+    Kategoriak_Inditas_Button = Button(
             image=img2,
             borderwidth=0,
             highlightthickness=0,
             command=btn_clicked,
             relief="flat")
 
-        Kategoriak_Inditas_Button.place(
+    Kategoriak_Inditas_Button.place(
             x=426, y=429,
             width=228,
             height=96)
 
-        window.resizable(False, False)
-        window.mainloop()
+    window.resizable(False, False)
+    window.mainloop()
 
 def felhasznalo_kerdes_szerk(): #Robi
     def selfDestroy():
